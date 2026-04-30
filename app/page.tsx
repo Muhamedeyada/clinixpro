10a31f287cb543917f9820df7b749406febd867a
mohamed eyada <mohamed.ayada.ma@gmail.com>
2026-04-30 16:18:18 +0300

    fix: uncomment PatientForm and PasskeyModal imports, update copyright year

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// import { PatientForm } from "@/components/forms/PatientForm";
-// import { PasskeyModal } from "@/components/PasskeyModal";
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
 const Home = ({ searchParams }: SearchParamProps) => {
   const isAdmin = searchParams?.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* {isAdmin && <PasskeyModal />} */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -21,11 +21,11 @@ const Home = ({ searchParams }: SearchParamProps) => {
             className="mb-12 h-10 w-fit"
           />
 
-          {/* <PatientForm /> */}
+          <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 ClinixPro
+              © 2026 ClinixPro
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
